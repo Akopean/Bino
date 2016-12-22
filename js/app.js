bd3749ee158335000b2117f0c66b1553f637aad8
Goog1e <agasi_dark@ukr.net>
2016-12-22 22:37:15 +0200

    init gh-pages

---
+++ js/app.js
@@ -0,0 +1,1 @@
+$(".portfolio-footer__element").hover(function(){$($(this)).children(".portfolio-element__info").removeClass("hidden")},function(){$(this).children(".portfolio-element__info").addClass("hidden")});
