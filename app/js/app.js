16e2ee822a3a9994180682dd2e356c81b1883720
Goog1e <agasi_dark@ukr.net>
2016-12-22 22:39:34 +0200

    udapte

--- app/js/app.js
+++
@@ -1,5 +0,0 @@
-$('.portfolio-footer__element').hover(function() {
-	$($(this)).children('.portfolio-element__info').removeClass('hidden');
-}, function() {
-	$(this).children('.portfolio-element__info').addClass('hidden');
-});
