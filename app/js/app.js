0165ec32c90fa98afefb698baa5ae1bf7dd5b299
Goog1e <agasi_dark@ukr.net>
2016-12-30 19:12:08 +0200

    update

--- app/js/app.js
+++ app/js/app.js
@@ -3,3 +3,30 @@ $('.portfolio-footer__element').hover(function() {
 }, function() {
 	$(this).children('.portfolio-element__info').addClass('hidden');
 });
+
+$('.team-member__block>.team-member').hover(function(){
+	$(this).stop(true);
+	$(this).animate({
+		width: 80,
+		height: 80,
+		borderRadius: 70,
+		boxShadow : "0 6px 16px 3px black",
+	},500);
+}, function(){
+	$(this).stop(true);
+	$(this).animate({
+		width: 57,
+		height: 57,
+		borderRadius: 30,
+
+	},500);
+});
+
+$(document).ready(function(){
+    $('a[href^="#"]').click(function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
